908204417d7482ff4570f8bd42dc52fe71054de6
Andrezza Hamburgo de Freire <andrezzahamburgo@gmail.com>
2024-04-13 18:26:04 -0300

    update scrap

--- web_scraping/scap.py
+++ web_scraping/scap.py
@@ -1,53 +1,30 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from tabulate import tabulate
-# import pandas as pd
-
-url = "https://fbref.com/pt/comps/24/2023/stats/2023-Serie-A-estatisticas"
-
-
-driver = webdriver.Chrome()
-
-driver.get(url)
-
-html_content = driver.page_source
-
-soup = BeautifulSoup(html_content, "html.parser")
-
-driver.quit()
+import pandas as pd
+import variaveis as v
 
 
-table = soup.find("table", id="stats_standard").find('tbody')
-# df = pd.read_html(str(table), decimal=',', thousands='.')[0]
-# print(df)
+url = v.url
+id = v.id_table
+df = pd.DataFrame()
+for site in url :
+    # index = str(url.index(site))
+    driver = webdriver.Chrome()
 
+    driver.get(site)
 
-if table:
+    html_content = driver.page_source
 
-  table_rows = table.find_all("tr")
+    soup = BeautifulSoup(html_content, "html.parser")
 
-  extracted_data = []
-  
-  for row in table_rows:
-    row_data = []  
+    driver.quit()
+
+    table = soup.find("table", id= id[url.index(site)])
+    df = pd.concat([df, pd.read_html(str(table), decimal=',', thousands='.')[0]], ignore_index=True)
     
-    data_cells = row.find_all("td")
-    if len(data_cells) == 0: continue
     
-    for cell in data_cells:
-      cell_text = cell.text.strip().replace(',', '.')  
-      row_data.append(cell_text)
-    extracted_data.append(row_data)
-
-  print(extracted_data)
- 
-else:
-  print("Table not found!")
-
-# Close the browser window
-
-
-with open('saida.csv', 'w') as out:
-  for lin in extracted_data:
-    out.write(','.join(lin) + '\n')
+df.to_csv('dados_jogadores.csv', index=False)
+    
+    
 
