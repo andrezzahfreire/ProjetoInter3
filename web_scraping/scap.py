120e889d5d55be171bdb38de1bf64fa9136d7952
FelipeBotelho94 <fbotelho94@gmail.com>
2024-04-23 20:45:19 -0300

    Muitas
    coisas

--- web_scraping/scap.py
+++ web_scraping/scap.py
@@ -10,7 +10,7 @@ url = v.url
 id = v.id_table
 df = pd.DataFrame()
 for site in url :
-    # index = str(url.index(site))
+    index = str(url.index(site))
     driver = webdriver.Chrome()
 
     driver.get(site)
@@ -24,16 +24,41 @@ for site in url :
     table = soup.find("table", id= id[url.index(site)])
     df = pd.concat([df, pd.read_html(str(table), decimal=',', thousands='.')[0]], ignore_index=True)
     df.head()
+    print(df)
     
-    # session = req.Session()
-    # for index, row in df.iterrows():
-    #     print(row[2])
-    #     dado = {
-    #         "nome": row[2]
-    #     }
-    #     session.post("http://localhost:8080/jogadores", dado)
+    session = req.Session()
+    for index, row in df.iterrows():
+        print(row[0])
+        dado = {
+            "jogador": row['Jogador'],  
+            "nacionalidade": row['Nação'],
+            "posicao": row['Pos.'],  
+            "equipe": row['Equipe'], 
+            "idade": row['Idade'], 
+            "nascimento": row['Nascimento'],
+            "jogosDisputados": row['MP'], # Jogos disputados
+            "inicios": row['Inícios'],  
+            "minutos": row['Min.'],   # Jogos disputados
+            "90s": row['90s'],        # 90 minutos jogados
+            "gols": row['Gols'],      # Gols marcados
+            "assistencias": row['Assis.'],  # Assistências
+            "gols_assistencias": row['G+A'],  # Gols + Assistências
+            "gols_penal": row['G-PB'],       # Gols de pênalti
+            "penaltis_batidos": row['PB'],   # Pênaltis batidos
+            "passes_tentados": row['PT'],    # Passes tentados
+            "cartoes_amarelos": row['CrtsA'],  # Cartões amarelos
+            "cartoes_vermelhos": row['CrtV'],  # Cartões vermelhos
+            "xG": row['xG'],         # Expected Goals (Gols esperados)
+            "npxG": row['npxG'],     # Non-penalty Expected Goals (Gols esperados sem pênaltis)
+            "xAG": row['xAG'],       # Expected Assists (Assistências esperadas)
+            "goleassis": row['npxG+xAG'],  # Expected Goals + Expected Assists (Gols esperados + Assistências esperadas)
+            "progressao_carregada": row['PrgC'],   # Progressão carregada
+            "progressao_passada": row['PrgP'],    # Progressão passada
+            "progressao_recebida": row['PrgR'],   # Progressão recebida
+        }
+    session.post("http://localhost:8080/jogadores", dado)
+    print(dado)
 
-# df.to_csv('dados_jogadores.csv', index=False)
     
     
 
