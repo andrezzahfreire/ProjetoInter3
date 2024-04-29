f531e92365d96811467919357d6e88d8c076d8ad
FelipeBotelho94 <fbotelho94@gmail.com>
2024-04-29 10:13:25 -0300

    Raspagem arrumada

--- web_scraping/scap.py
+++ web_scraping/scap.py
@@ -1,69 +1,68 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
-from tabulate import tabulate
-import pandas as pd
-import variaveis as v
-import requests as req
-import json
+# IMPORTACAO DE MODULOS
+from bs4 import BeautifulSoup as bs # modulo para processar html
+import pandas as pd # mexe com tabelas
+import requests # acessa urls e extrai o codigo deles
+import json # processa dicionarios em formato acessivel para sites ou progWeb
 
+# LISTAS DE URLS E IDS DAS TABELAS
+urls = ["https://fbref.com/pt/comps/24/2023/stats/2023-Serie-A-estatisticas", 
+        "https://fbref.com/pt/comps/24/2023/passing/2023-Serie-A-estatisticas", 
+        "https://fbref.com/pt/comps/24/2023/keepersadv/2023-Serie-A-estatisticas", 
+        "https://fbref.com/pt/comps/24/2023/shooting/2023-Serie-A-estatisticas", 
+        "https://fbref.com/pt/comps/24/2023/passing/2023-Serie-A-estatisticas", 
+        "https://fbref.com/pt/comps/24/2023/passing_types/2023-Serie-A-estatisticas", 
+        "https://fbref.com/pt/comps/24/2023/gca/2023-Serie-A-estatisticas", 
+        "https://fbref.com/pt/comps/24/2023/possession/2023-Serie-A-estatisticas", 
+        "https://fbref.com/pt/comps/24/2023/playingtime/2023-Serie-A-estatisticas", 
+        "https://fbref.com/pt/comps/24/2023/misc/2023-Serie-A-estatisticas"]
 
-url = v.url
-id = v.id_table
-df = pd.DataFrame()
-for site in url :
-    index = str(url.index(site))
-    driver = webdriver.Chrome()
+table_ids = ["stats_standard", 
+             "stats_passing", 
+             "stats_keeper_adv", 
+             "stats_shooting", 
+             "stats_passing", 
+             "stats_passing_types", 
+             "stats_gca", 
+             "stats_possession", 
+             "stats_playing_time", 
+             "stats_misc"]
 
-    driver.get(site)
+sesh = requests.Session() # inicializa uma sessao no modulo requests
 
-    html_content = driver.page_source
-
-    html_content = html_content.replace('>Pos.<', '>Posicao<')
-    soup = BeautifulSoup( html_content, "html.parser")
-
-    driver.quit()
-
-    table = soup.find("table", id= id[url.index(site)])
-    df = pd.concat([df, pd.read_html(str(table), decimal=',', thousands='.', header=1)[0]], ignore_index=True)
-    df = df.loc[df['Class.'] != 'Class.']
-    df.to_csv('scrap.csv', index=False)
+for i, url in enumerate(urls): # itera sobre a lista de urls, devolve tuplas (INDICE, VALOR) da lista
     
-    headers = {
-        'Content-type': 'application/json'
-    }
-    session = req.Session()
-    for (index, row) in df.iterrows():
-        print(row)
-        dado = {
-            "jogador": row['Jogador'],  
-            "nacionalidade": row['Nação'],
-            "posicao": row['Posicao'],  
-            "equipe": row['Equipe'], 
-            "idade": row['Idade'], 
-            "nascimento": row['Nascimento'],
-            "jogosDisputados": row['MP'], # Jogos disputados
-            "inicios": row['Inícios'],  
-            "minutos": row['Min.'],   # Jogos disputados
-            "90s": row['90s'],        # 90 minutos jogados
-            "gols": row['Gols'],      # Gols marcados
-            "assistencias": row['Assis.'],  # Assistências
-            "gols_assistencias": row['G+A'],  # Gols + Assistências
-            "gols_penal": row['G-PB'],       # Gols de pênalti
-            "penaltis_batidos": row['PB'],   # Pênaltis batidos
-            "passes_tentados": row['PT'],    # Passes tentados
-            "cartoes_amarelos": row['CrtsA'],  # Cartões amarelos
-            "cartoes_vermelhos": row['CrtV'],  # Cartões vermelhos
-            "xG": row['xG'],         # Expected Goals (Gols esperados)
-            "npxG": row['npxG'],     # Non-penalty Expected Goals (Gols esperados sem pênaltis)
-            "xAG": row['xAG'],       # Expected Assists (Assistências esperadas)
-            "goleassis": row['npxG+xAG'],  # Expected Goals + Expected Assists (Gols esperados + Assistências esperadas)
-            "progressao_carregada": row['PrgC'],   # Progressão carregada
-            "progressao_passada": row['PrgP'],    # Progressão passada
-            "progressao_recebida": row['PrgR'],   # Progressão recebida
-        }
-        session.post(
-            "http://localhost:8080/jogadores", json=json.dumps(dado)
-        )
-        print(dado)
+    # pega o codigo html da url e remove marcadores de comentarios, transformando todo comentario em 'parte do codigo' <-- A TABELA TAVA ESCONDIDA NOS COMENTARIOS    
+    response = requests.get(url).text.replace('<!--', '').replace('-->', '')
     
+    # transforma o codigo html em um objeto BeautifulSoup, que permite a busca de elementos html
+    soup = bs(response, 'html.parser')
     
+    # no objeto BS, procura a tabela com o id especificado na lista de ids com o mesmo indice do URL
+    table_w_data = soup.select_one(f'table#{table_ids[i]}')
+    
+    # usa PANDAS para converter o HTML da tabela em uma DATAFRAME python
+    df = pd.read_html(str(table_w_data), header=1, encoding='latin1')[0]
+    
+    # pega os nomes (headers) das colunas da tabela (.columns) e converte em uma lista (.tolist())
+    dfheaders = df.columns.tolist()
+    
+    # roubei da net :)  remove os headers duplicados magicamente, n sei como funciona
+    df1 = df[df.iloc[:, 0] != df.columns[0]]
+    
+    # iterando sobre as linhas do dataframe
+    for (index, row) in df1.iterrows():
+        
+        # converte a linha da tabela em uma lista de valores
+        row = row.array
+        
+        # inicializa um dicionario vazio
+        dado = {}
+        
+        # itera sobre os elementos da linha, devolvendo tuplas (INDICE, VALOR) da lista
+        # PASSO A PASSO:
+        for j, element in enumerate(row): # PARA CADA (numero do elemento & elemento) NA LINHA
+            dado.update({dfheaders[j]: element}) # ADICIONA UMA NOVA CHAVE NO DICIONARIO, SENDO {listaDeHeaders[numeroDoElemento]: elementoDaLinha}
+
+        print(json.dumps(dado, ensure_ascii=False))
+        # POSTA O DICIONARIO NO SERVIDOR
+        #sesh.post("http://localhost:8080/jogadores", json=json.dumps(dado))
